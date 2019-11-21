b09a0be5044580486d37868ca556a95e5256d943
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-11-21 02:23:34 -0800

    Correct some reference terminology.

--- alpha.c
+++ alpha.c
@@ -58,23 +58,23 @@ void AlphaBltSSE(unsigned char *dst, unsigned char *src, int w, int h)
 // For each pixel: dst = (src_alpha * (src-dst)  + dst * 256) / 256
 		mov			edi,dst				// Move the address of index 0 of dst (unsigned char*(Because this is a pointer, it's an int, being compiled as x86, so 32 bits, or 4 bytes)) into EDI destination index register, for string operations
 		mov			esi,src				// Move the address of index 0 of src (unsigned char*(Because this is a pointer, it's an int, being compiled as x86, so 32 bits, or 4 bytes)) into ESI source index register, for string operations
-		mov			edx,h				// Move the address of h (int, 32 bits(4 bytes)) into 32-bit(4 byte) EDX register.
+		mov			edx,h				// Move the address of h (int, 32 bits(4 bytes)) into 32-bit(4 byte) EDX register. EDX now points to h.
 		pxor		mm6,mm6				// Performs a logical XOR operation on mm6 and mm6, then stores the result in mm6, a 64 bit MMX register. Because XORing itself, this initializes to 0.
 		pxor		mm7,mm7				// Performs a logical XOR operation on mm7 and mm7, then stores the result in mm7, a 64 bit MMX register. Because XORing itself, this initializes to 0.
 		xor			eax,eax				// Performs a logical XOR operation on eax and eax, then stores the result in eax, a 32 bit x86 register. Because XORing itself, this initializes to 0.
 scan_loop:								// Label this line to be jumped to.
-		mov			ecx,w				// Move address of w into ECX 32 bit(4 byte) register. ECX is our count register, it will decrement while looping.
+		mov			ecx,w				// Move address of w into ECX 32 bit(4 byte) register. ECX is our count register, it will decrement while looping. ECX now points to w.
 		xor			ebx,ebx				// Performs a logical XOR operation on EBX and EBX, then stores the result in EBX, a 32 bit x86 register. Because XORing itself, this initializes to 0.
 pix_loop:								// Label this line to be jumped to.
 		movq		mm4,[esi+ebx*8]		// mm0 = src (RG BA RG BA) // Moves a quadword(8 bytes) of data from address of esi+ebx*8 to mm4. ESI is src's starting address+EBX which is an offset counter, of 8 bytes. EBX is a counter.
 		movq		mm5,[edi+ebx*8]		// mm1 = dst (RG BA RG BA) // Moves a quadword(8 bytes) of data from address of edi+ebx*8 to mm5. ESD is dst's starting address+EBX which is an offset counter, of 8 bytes. EBX is a counter.
 // FIRST PIXEL
-		movq		mm0,mm4				// mm0 = 00 00 RG BA // Moves the addresses of a quadword(8 bytes: RGBA(Haven't checked the order. TGA is often BGRA though.), each channel is 2 bytes, 0-255.) from mm4 to mm0
-		movq		mm1,mm5				// mm1 = 00 00 RG BA // Moves the addresses of a quadword(8 bytes: RGBA(Haven't checked the order. TGA is often BGRA though.), each channel is 2 bytes, 0-255.) from mm5 to mm1
+		movq		mm0,mm4				// mm0 = 00 00 RG BA // Moves the quadword(8 bytes) contents of mm4 into mm0. This will be RGBA, with each channel being 2 bytes, with a value of 0-255.
+		movq		mm1,mm5				// mm1 = 00 00 RG BA // Moves the quadword(8 bytes) contents of mm4 into mm0. This will be RGBA, with each channel being 2 bytes, with a value of 0-255.
 		punpcklbw	mm0,mm6				// mm0 = (0R 0G 0B 0A) // http://qcd.phys.cmu.edu/QCDcluster/intel/vtune/reference/vc265.htm Explain this more specifically.
 		punpcklbw	mm1,mm7				// mm0 = (0R 0G 0B 0A) // http://qcd.phys.cmu.edu/QCDcluster/intel/vtune/reference/vc265.htm Explain this more specifically.
 		pshufw		mm2,mm0,0ffh		// mm2 = 0A 0A 0A 0A   // http://tommesani.com/index.php/component/content/article/2-simd/36-sse-primer.html Explain this more specifically.
-		movq		mm3,mm1				// mm3 = mm1		  // Moves the address of a quadword(8 bytes) from address of mm1 to mm3
+		movq		mm3,mm1				// mm3 = mm1		  // Moves the quadword(8 bytes) contents of mm1 to mm3.
 		psubw		mm0,mm1				// mm0 = mm0 - mm1	  // https://www.felixcloutier.com/x86/psubb:psubw:psubd Explain this more specifically.
 		psllw		mm3,8				// mm3 = mm1 * 256	  // https://www.felixcloutier.com/x86/psllw:pslld:psllq Explain this more specifically.
 		pmullw		mm0,mm2				// mm0 = (src-dst)*alpha // https://docs.oracle.com/cd/E19120-01/open.solaris/817-5477/eojdc/index.html Explain this more specifically.
@@ -83,7 +83,7 @@ pix_loop:								// Label this line to be jumped to.
 // SECOND PIXEL
 		punpckhbw	mm5,mm7				// mm5 = (0R 0G 0B 0A) // https://www.felixcloutier.com/x86/punpckhbw:punpckhwd:punpckhdq:punpckhqdq Explain this more specifically.
 		punpckhbw	mm4,mm6				// mm4 = (0R 0G 0B 0A) // https://www.felixcloutier.com/x86/punpckhbw:punpckhwd:punpckhdq:punpckhqdq Explain this more specifically.
-		movq		mm3,mm5				// mm3 = mm5		  // Moves the address of a quadword(8 bytes) from address of mm5 to mm3
+		movq		mm3,mm5				// mm3 = mm5		  // Moves the quadword(8 bytes) contents of mm5 to mm3.
 		pshufw		mm2,mm4,0ffh		// mm2 = 0A 0A 0A 0A  // https://www.felixcloutier.com/x86/pshufw Explain this more specifically.
 		psllw		mm3,8				// mm3 = mm5 * 256		// https://www.felixcloutier.com/x86/psllw:pslld:psllq Explain this more specifically.
 		psubw		mm4,mm5				// mm4 = mm4 - mm5		// https://www.felixcloutier.com/x86/psubb:psubw:psubd Explain this more specifically.
@@ -91,11 +91,11 @@ pix_loop:								// Label this line to be jumped to.
 		paddw		mm4,mm3				// mm4 = (src-dst)*alpha+dst*256 // https://www.felixcloutier.com/x86/paddb:paddw:paddd:paddq Explain this more specifically.
 		psrlw		mm4,8				// mm4 = ((src - dst) * alpha + dst * 256) / 256 // https://www.felixcloutier.com/x86/psrlw:psrld:psrlq Explain this more specifically.
 		packuswb	mm0,mm4				// mm0 = RG BA RG BA // https://www.felixcloutier.com/x86/packuswb Explain this more specifically.
-		movq		[edi+ebx*8],mm0		// dst = mm0		// Moves a quadword(8 bytes) of data from address of mm0 to address indicated by edi+ebx*8. EDI is dst's starting address+EBX which is an offset counter, of 8 bytes. EBX is a counter.
+		movq		[edi+ebx*8],mm0		// dst = mm0		// Moves the quadword(8 bytes) contents of mm0 to address indicated by edi+ebx*8. EDI is dst's starting address+EBX which is an offset counter, of 8 bytes. EBX is a counter.
 		inc			ebx					// increment the value stored at the address of ebx	// Increment EBX
 		loop		pix_loop			// loop back to pix_loop label. https://docs.oracle.com/cd/E19455-01/806-3773/instructionset-72/index.html Loop also decrements ECX, and continues without looping if that register is zero.
 //
-		mov			ebx, wmul4			// Move the address of wmul4 into ebx
+		mov			ebx, wmul4			// Move the address of wmul4 into ebx, creating a pointer to wmul4 in ebx.
 		add			esi, ebx			// Add the contents of EBX into ESI
 		add			edi, ebx			// Add the contents of EBX into EDI
 		dec			edx					// Decrement EDX
@@ -116,19 +116,19 @@ void AlphaBltMMX(unsigned char *dst, unsigned char *src, int w, int h)
 // For each pixel: dst = (src_alpha * (src-dst)  + dst * 256) / 256
 		mov			edi,dst				// Move the address of index 0 of dst (unsigned char*(Because this is a pointer, it's an int, being compiled as x86, so 32 bits, or 4 bytes)) into EDI destination index register, for string operations
 		mov			esi,src				// Move the address of index 0 of src (unsigned char*(Because this is a pointer, it's an int, being compiled as x86, so 32 bits, or 4 bytes)) into ESI source index register, for string operations
-		mov			edx,h				// Move the address of h (int, 32 bits(4 bytes)) into 32-bit(4 byte) EDX register.
+		mov			edx,h				// Move the address of h (int, 32 bits(4 bytes)) into 32-bit(4 byte) EDX register. EDX now points to h.
 		pxor		mm6,mm6				// Performs a logical XOR operation on mm6 and mm6, then stores the result in mm6, a 64 bit MMX register. Because XORing itself, this initializes to 0.
 		pxor		mm7,mm7				// Performs a logical XOR operation on mm7 and mm7, then stores the result in mm7, a 64 bit MMX register. Because XORing itself, this initializes to 0.
 		xor			eax,eax				// Performs a logical XOR operation on eax and eax, then stores the result in eax, a 32 bit x86 register. Because XORing itself, this initializes to 0.
 scan_loop:
-		mov			ecx,w				// Move address of w into ECX 32 bit(4 byte) register. ECX is our count register, it will decrement while looping.
+		mov			ecx,w				// Move address of w into ECX 32 bit(4 byte) register. ECX is our count register, it will decrement while looping. ECX now points to w.
 		xor			ebx,ebx				// Performs a logical XOR operation on EBX and EBX, then stores the result in EBX, a 32 bit x86 register. Because XORing itself, this initializes to 0.
 pix_loop:
 		movq		mm4,[esi+ebx*8]		// mm4 = src (RG BA RG BA) // Moves a quadword(8 bytes) of data from address of esi+ebx*8 to mm4. ESI is src's starting address+EBX which is an offset counter, of 8 bytes. EBX is a counter.
 		movq		mm5,[edi+ebx*8]		// mm5 = dst (RG BA RG BA) // Moves a quadword(8 bytes) of data from address of edi+ebx*8 to mm5. ESD is dst's starting address+EBX which is an offset counter, of 8 bytes. EBX is a counter.
 // FIRST PIXEL
-		movq		mm0,mm4				// mm0 = src (-- -- RG BA) // Moves the addresses of a quadword(8 bytes: RGBA(Haven't checked the order. TGA is often BGRA though.), each channel is 2 bytes, 0-255.) from mm4 to mm0
-		movq		mm1,mm5				// mm1 = dst (-- -- RG BA) // Moves the addresses of a quadword(8 bytes: RGBA(Haven't checked the order. TGA is often BGRA though.), each channel is 2 bytes, 0-255.) from mm5 to mm1
+		movq		mm0,mm4				// mm0 = src (-- -- RG BA) // Moves the quadword(8 bytes) contents of mm4 into mm0. This will be RGBA, with each channel being 2 bytes, with a value of 0-255.
+		movq		mm1,mm5				// mm1 = dst (-- -- RG BA) // Moves the quadword(8 bytes) contents of mm4 into mm0. This will be RGBA, with each channel being 2 bytes, with a value of 0-255.
 		punpcklbw	mm0,mm6				// mm0 = (0R 0G 0B 0A)
 		mov			al,[esi+ebx*8+3]	// eax = pixel alpha (0 - 255)
 		punpcklbw	mm1,mm7				// mm1 = (0R 0G 0B 0A)
@@ -161,7 +161,7 @@ pix_loop:
 		inc			ebx					// increment the value stored at the address of ebx	// Increment EBX
 // REPEAT
 		loop		pix_loop			// loop back to pix_loop label. https://docs.oracle.com/cd/E19455-01/806-3773/instructionset-72/index.html Loop also decrements ECX, and continues without looping if that register is zero.
-		mov			ebx, wmul4			// Move the address of wmul4 into ebx
+		mov			ebx, wmul4			// Move the address of wmul4 into ebx, creating a pointer to wmul4 in ebx.
 		add			esi, ebx			// Add the contents of EBX into ESI
 		add			edi, ebx			// Add the contents of EBX into EDI
 		dec			edx					// Decrement EDX
